c7688428e1ca108eeacbeb6b33c49fa74e6273af
Daniel Bidulock <daniel@capitolhill.ca>
2014-02-10 22:48:23 -0700

    Enable gebo-client-token authentication

--- root/app/scripts/controllers/main.js
+++ root/app/scripts/controllers/main.js
@@ -1,10 +1,73 @@
 'use strict';
 
-angular.module('geboHai')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+angular.module('geboHai').
+    controller('MainCtrl', function ($scope, Token, $location) {
+
+        $scope.verified = false;
+        $scope.admin = false;
+
+        /**
+         * Configure OAuth2 for interaction with gebo-server
+         */
+        var baseUrl = window.location.origin;
+
+        Token.setEndpoints({
+            clientId: 'gebo-hai@capitolhill.ca',
+            clientName: 'gebo-registrant-hai',
+            gebo: 'https://localhost:3443',
+            localStorageName: '{%= name %}-token',
+            redirect: baseUrl + '/components/gebo-client-token/dist/oauth2callback.html',
+          });
+        
+        /**
+         * See if this client already has a token 
+         */
+        $scope.accessToken = Token.get();
+        
+        if ($scope.accessToken) {
+          Token.verify($scope.accessToken).
+            then(function(data) {
+                    $scope.agentName = data.name;
+                    $scope.verified = true;
+                    $scope.admin = data.admin;
+                }, function() {
+                    window.alert('You have an expired or invalid token.');
+              });
+        }
+
+        /**
+         * Allow gebo-client access to the gebo user's resources
+         */
+        $scope.authenticate = function() {
+            var extraParams = {};
+            Token.getTokenByPopup(extraParams)
+                .then(function(params) {
+                    // Success
+                    Token.verify(params.access_token).
+                    then(function(data) {
+                            $scope.accessToken = params.access_token;
+                            $scope.agentName = data.name;
+                            $scope.verified = true;
+                            $scope.admin = data.admin;
+                            Token.set(params.access_token);
+                        }, function() {
+                            window.alert('Cannot verify token.');
+                      });
+                    }, function() {
+                        // Failure
+                        window.alert('Failed to get token from popup.');
+                    });
+          };
+                    
+        /**
+         * Disallow gebo-client access to the gebo user's resources
+         */
+        $scope.deauthenticate = function () {
+            delete $scope.agentName;
+            delete $scope.accessToken;
+            $scope.verified = false;
+            $scope.admin = false;
+            Token.clear();
+            $location.path('/');
+        };
+  });
