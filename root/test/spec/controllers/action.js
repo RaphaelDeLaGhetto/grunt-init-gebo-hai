115a2e1b9c0cad8c5a05c13a73569daff11c76a3
Daniel Bidulock <daniel@capitolhill.ca>
2014-03-13 10:35:58 -0600

    Correct app name in ActionCtrl tests

--- root/test/spec/controllers/action.js
+++ root/test/spec/controllers/action.js
@@ -3,7 +3,7 @@
 describe('Controller: ActionCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('nameApp'));
+  beforeEach(module('geboHai'));
 
   var ActionCtrl,
     scope;
